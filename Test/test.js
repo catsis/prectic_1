00d527448eb49dbd0d2633b73d5aea4d03978841
catsis <bot09882565@gmail.com>
2021-10-27 03:13:10 +0800

    10/27

--- Test/test.js
+++ Test/test.js
@@ -1,16 +1,27 @@
-var hello = function(){
-    console.log("Hello!!")
-}
 
-hello()
+// var hello = function(){
+//     console.log("Hello!!")
+// }
+
+// hello()
 
 function hello2(n){
     for (var i = 1; i <= n; i++){
-        if (!(i%2))continue
+        if (i%2)continue
             console.log("Hello2!!", i)
     }
 }
 
 hello2(20)
 
-//hello()
+// var arr = [1, 2, 3, 4, 5]
+
+// for (var b = 0; b <= arr.length; b++){
+//     arr.push(b)
+// }
+
+function LOL(x){
+    return x*2
+} 
+
+console.log(LOL(5))
