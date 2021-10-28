f2029aed86ea9f7a25552e36bd5ed6d1e6c3478c
catsis <bot09882565@gmail.com>
2021-10-28 17:44:55 +0800

    10/28

--- Test/test.js
+++ Test/test.js
@@ -5,23 +5,26 @@
 
 // hello()
 
-function hello2(n){
-    for (var i = 1; i <= n; i++){
-        if (i%2)continue
-            console.log("Hello2!!", i)
+// function hello2(n){
+//     for (var i = 1; i <= n; i++){
+//         if (i%2)continue
+//             console.log("Hello2!!", i)
+//     }
+// }
+
+// hello2(20)
+
+function transform(arr, num){
+    var result = []
+    for (var b = 0; b < arr.length; b++){
+        result.push(num(arr[b]))
     }
+    return result
 }
 
-hello2(20)
-
-// var arr = [1, 2, 3, 4, 5]
-
-// for (var b = 0; b <= arr.length; b++){
-//     arr.push(b)
-// }
 
 function LOL(x){
     return x*2
 } 
 
-console.log(LOL(5))
+console.log(transform([2, 4, 6, 8], LOL))
---
+++ Test/test2.js
@@ -0,0 +1,15 @@
+
+function add(a, b){
+    var temp = a
+    a = b
+    b = temp
+    console.log(a, b)
+}
+
+var num1 = 10
+var num2 = 20
+
+
+add(5, 6)
+
+console.log(num1, num2)
